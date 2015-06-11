2a7e4266d8deb1d5ada94091fc23ef01805ac2c9
Tero Keski-Valkama <tero.keski-valkama@cybercom.com>
2015-06-11 20:35:02 +0300

    First draft implementstion.

--- src/main/java/fi/neter/confluence/social/ChildTopicPagesModule.java
+++ src/main/java/fi/neter/confluence/social/ChildTopicPagesModule.java
@@ -1,36 +1,62 @@
 package fi.neter.confluence.social;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
-import com.atlassian.confluence.api.service.content.*;
 import com.atlassian.confluence.content.render.xhtml.ConversionContext;
+import com.atlassian.confluence.core.ContentEntityObject;
 import com.atlassian.confluence.macro.Macro;
 import com.atlassian.confluence.macro.MacroExecutionException;
+import com.atlassian.confluence.pages.Comment;
+import com.atlassian.confluence.pages.Page;
 
 public class ChildTopicPagesModule implements Macro
 {
-	private ContentService contentService;
-	
-    public ChildTopicPagesModule(ContentService contentService) {
-        this.contentService = contentService;
-    }
-
 	@Override
 	public String execute(Map<String, String> parameters, String body,
 			ConversionContext context) throws MacroExecutionException {
-		// TODO Auto-generated method stub
-		return null;
+        ContentEntityObject ceo = context.getPageContext().getEntity();
+        Page parent = (Page) ceo ;
+        List<Page> children = parent.getChildren();
+        StringBuilder html = new StringBuilder();
+        // Title row.
+        html.append("<table class=\"childtopicpages\"><hr><hd>Aihe</hd><hd>Viestejä</hd><hd>Viimeinen viesti</hd></hr>");
+        for (Page child : children) {
+        	List<Comment> comments = child.getComments();
+    		
+        	int numberOfComments = comments.size();
+        	Date latestCommentDate = null;
+        	for (Comment comment : comments) {
+        		Date lastModificationDate = comment.getLastModificationDate();
+        		Date creationDate = comment.getCreationDate();
+        		if (latestCommentDate == null ||
+        				(lastModificationDate != null && latestCommentDate.before(lastModificationDate))) {
+        			latestCommentDate = lastModificationDate;
+        		}
+        		if (latestCommentDate == null ||
+        				(creationDate != null && latestCommentDate.before(creationDate))) {
+        			latestCommentDate = creationDate;
+        		}
+        	}
+        	DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
+        	html.append("<tr><td>" + child.getLinkWikiMarkup() + "</td><td>"
+        	    + numberOfComments + "</td><td>" + df.format(latestCommentDate) + "</td></tr>");
+        }
+        // Trailer.
+        html.append("</table>");
+		return html.toString();
 	}
 
 	@Override
 	public BodyType getBodyType() {
-		// TODO Auto-generated method stub
-		return null;
+		return BodyType.RICH_TEXT;
 	}
 
 	@Override
 	public OutputType getOutputType() {
-		// TODO Auto-generated method stub
-		return null;
+		return OutputType.BLOCK;
 	}
 }
